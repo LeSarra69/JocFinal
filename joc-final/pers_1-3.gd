6d91bf62e74ecc236426561ea7d44f1f9e68bc07
Lebronalex13 <alexbilbao08@gmail.com>
2025-05-15 00:04:57 +0200

    1

---
+++ joc-final/pers_1-3.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+const velocitat:= 200
+const vsalt = -400
+const bala = preload("res://areabala.tscn")
+var gravetat = ProjectSettings.get_setting("physics/2d/default_gravity")
+var miradret =true
+
+func _physics_process(delta):
+
+	if Input.is_action_just_pressed("fsalt") and is_on_floor():
+		velocity.y = 1.1 * vsalt
+
+	var direccio = Vector2.ZERO
+	direccio.x = Input.get_axis("fesquerra", "fdavant")
+	
+	
+	if (Input.is_action_pressed("fdavant") or Input.is_action_pressed("fesquerra")) and is_on_floor():
+		$AnimatedSprite2D.play("animaico_moviment")
+	elif is_on_floor() == false:
+		$AnimatedSprite2D.play("animacio_roda")
+	else: 
+		$AnimatedSprite2D.play("quiet")
+	if Input.is_action_pressed("fdavant"):
+		$AnimatedSprite2D.flip_h = false
+		miradret = true
+	if Input.is_action_pressed("fesquerra"):
+		$AnimatedSprite2D.flip_h = true
+		miradret = false
+	velocity.x = direccio.x * velocitat
+	velocity.y += gravetat * delta
+	
+	#disparar
+	var shoot = bala.instantiate()
+	if Input.is_action_just_pressed("fbala"):
+		get_parent().add_child(shoot)
+		if miradret:
+			shoot.position = $spownpoint1.global_position
+		if not miradret:
+			shoot.position = $spownpoint2.global_position
+			shoot.scale.x *=-1
+			shoot.vbala *=-1
+			
+	
+	move_and_slide()
---
+++ joc-final/personatge_2-3.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+const velocitat:= 170
+const vsalt = -300
+
+var gravetat = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+
+	if Input.is_action_just_pressed("salt") and is_on_floor():
+		velocity.y = vsalt
+
+	var direccio = Vector2.ZERO
+	direccio.x = Input.get_axis("esquerra", "dreta")
+	
+	
+	if (Input.is_action_pressed("dreta") or Input.is_action_pressed("esquerra")) and is_on_floor():
+		$AnimatedSprite2D.play("default")
+	elif is_on_floor() == false:
+		$AnimatedSprite2D.play("salt2")
+	else: 
+		$AnimatedSprite2D.play("nquiet")
+	if Input.is_action_pressed("dreta"):
+		$AnimatedSprite2D.flip_h = false
+	if Input.is_action_pressed("esquerra"):
+		$AnimatedSprite2D.flip_h = true
+	velocity.x = direccio.x * velocitat
+	velocity.y += gravetat * delta
+	
+	move_and_slide()
+
+
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	get_tree().change_scene_to_file("res://game over.tscn")
+
+
+func _on_timer_bar_time_to_die() -> void:
+	get_tree().change_scene_to_file("res://game over.tscn")
+
+
+func _on_area_impacte_area_entered(area: Area2D) -> void:
+	Globalingo.vides -=1
+	print(Globalingo.vides)
